2d537b3ea920eeb4648ca05faf8331d23b112f03
krvcz <sanko.sebastian@gmail.com>
2022-11-28 22:07:18 +0100

    First init

---
+++ ideas100/src/main/java/pl/sebastian/ideas/model/Answer.java
@@ -0,0 +1,22 @@
+package pl.sebastian.ideas.model;
+
+public class Answer {
+
+    private String name;
+
+    public Answer() {
+    }
+
+    public Answer(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
